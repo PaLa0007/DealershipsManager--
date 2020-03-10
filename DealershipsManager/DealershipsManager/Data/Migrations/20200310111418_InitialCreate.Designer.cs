56e17a73658bc7016e9d0ab641f7b0eb0ecf6015
veli_vasilev_ <veli_vasilev_@abv.bg>
2020-03-10 13:15:21 +0200

    Made first Migration plus updated database

--- DealershipsManager/DealershipsManager/Data/Migrations/20200310111418_InitialCreate.Designer.cs
+++ DealershipsManager/DealershipsManager/Data/Migrations/20200310111418_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using DealershipsManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DealershipsManager.Data.Migrations
 {
     [DbContext(typeof(DealershipsManagerDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200310111418_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
