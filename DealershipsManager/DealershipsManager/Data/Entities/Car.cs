2cc112ce2d8e8fdc50fb653c93bae940271428e3
veli_vasilev_ <veli_vasilev_@abv.bg>
2020-03-10 13:05:13 +0200

    Created entities

---
+++ DealershipsManager/DealershipsManager/Data/Entities/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DealershipsManager.Data.Entities
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        public TypeEnum Type { get; set; }
+
+        public EngineEnum Engine { get; set; }
+
+        public string Horsepower { get; set; }
+
+        public TransmissionEnum Transmission { get; set; }
+
+        public byte Gears { get; set; }
+
+        public string Color { get; set; }
+
+        public double Price { get; set; }
+
+        public bool IsSold { get; set; }
+
+
+
+        public int DealershipId { get; set; }
+
+        public virtual Dealership Dealership { get; set; }
+    }
+}
---
+++ DealershipsManager/DealershipsManager/Data/Entities/Dealership.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DealershipsManager.Data.Entities
+{
+    public class Dealership
+    {
+        public int DealershipId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public string Town { get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; }
+
+        public Dealership()
+        {
+            this.Cars = new HashSet<Car>();
+        }
+    }
+}
