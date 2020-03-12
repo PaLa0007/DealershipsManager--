a639319546157741fd8aaf6e41b86c7248fcc0ff
veli_vasilev_ <veli_vasilev_@abv.bg>
2020-03-12 20:41:41 +0200

    Created Car filter

---
+++ DealershipsManager/DealershipsManager/Models/Filters/FilterCarViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DealershipsManager.Data.Enumeration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DealershipsManager.Models.Filters
+{
+    public class FilterCarViewModel
+    {
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        public TypeEnum Type { get; set; }
+
+        public int Horsepower { get; set; }
+
+        public TransmissionEnum Transmission { get; set; }
+
+        public string Color { get; set; }
+
+        public double Price { get; set; }
+    }
+}
