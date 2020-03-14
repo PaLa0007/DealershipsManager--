87d0a0bb35508a199aa14cd9a48d3b431b1a235a
veli_vasilev_ <veli_vasilev_@abv.bg>
2020-03-14 22:09:06 +0200

    Created EditUserViewModel.cs

---
+++ DealershipsManager/DealershipsManager/Models/User/EditUserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DealershipsManager.Models.User
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PersonalNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool IsAdministrator { get; set; }
+    }
+}
